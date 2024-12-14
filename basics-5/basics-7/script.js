ab98da712fa226a0b83ab2d0fff9684e3c0a5eb5
Sebastian Ekström <SebastiaN___@live.se>
2024-12-14 11:57:51 +0100

    Variables and consts

---
+++ basics-5/basics-7/script.js
@@ -0,0 +1,25 @@
+// Variables & constants
+
+let age = 25; // variabeln = age och innehåller nummer 25
+let year = 2019;
+
+
+console.log(age, year)
+
+age = 30;  // Variabeln 'age' har nu fått ett uppdaterat värde av 30 istället för 25
+            // output i consol.log = 25 2019 följt av ny rad med 30
+
+console.log(age);
+
+
+const  points = 100;  // En const/konstant kan inte om deklarer variablen den är tilldelad
+//points = 50;
+
+console.log(points)
+
+var score = 75;  // var är den äldre varianten att deklarera variabler innan LET och CONST.
+console.log(score);
+
+/**
+ * https://www.w3schools.com/js/js_reserved.asp // JS keywords
+ */
