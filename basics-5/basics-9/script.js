3de6a1d41f692cf1cb20657dbb4c4a43f8f683f9
Sebastian Ekström <SebastiaN___@live.se>
2024-12-14 12:11:16 +0100

    added branch basics-9 incl html and JS file

---
+++ basics-5/basics-9/script.js
@@ -0,0 +1,12 @@
+// JavaScript Data types
+/**
+ * Number - 1,2,3
+ * String -'hello world', "hotmail.se"
+ * Boolean - true / false
+ * Null -Explecit set a variable with no value
+ * Undefined - For variables that have not yet been defined
+ * Object - Complex data structures - Arrays, Dates, Literals etc
+ * Symbol - Used with objects
+ */
+
+
